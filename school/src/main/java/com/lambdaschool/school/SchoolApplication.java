57c40d2ecbac50ab04449de7d46d1542c7248f11
rushi444 <rushi44@gmail.com>
2019-07-22 14:55:22 -0700

    Added DispatcherServlet to main App

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -2,14 +2,20 @@ package com.lambdaschool.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
-public class SchoolApplication
-{
+public class SchoolApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SchoolApplication.class, args);
 
-    public static void main(String[] args)
-    {
-        SpringApplication.run(SchoolApplication.class, args);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
